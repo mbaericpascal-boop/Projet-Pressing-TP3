60ca5655ca4791a2089221fee627edbc9b8477ab
john2005-dev <jonathanngongang67@gmail.com>
2026-05-13 09:54:08 +0100

    Fix: chemin SQLite absolu via user.home + création auto du dossier

--- pressing/src/main/java/com/tp/pressing/TpPressingApplication.java
+++ pressing/src/main/java/com/tp/pressing/TpPressingApplication.java
@@ -2,13 +2,19 @@ package com.tp.pressing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import java.io.File;
 
 @SpringBootApplication
-@EnableAsync
 public class TpPressingApplication {
 
     public static void main(String[] args) {
+        // Créer le dossier de la base de données s'il n'existe pas
+        File dbDir = new File(System.getProperty("user.home") + "/pressing-data");
+        if (!dbDir.exists()) {
+            dbDir.mkdirs();
+            System.out.println("✅ Dossier BD créé : " + dbDir.getAbsolutePath());
+        }
+
         SpringApplication.run(TpPressingApplication.class, args);
     }
 }
